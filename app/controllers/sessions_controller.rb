ce0b622c641962421b74898bfd253d3fbba1d3f2
rhinod <isc.damians@gmail.com>
2020-05-28 20:57:26 -0700

    Restrict access in controller level

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+    
+    before_action :logged_in_redirect, only: [:new, :create]
+    
     def new
     end
 
@@ -18,6 +21,15 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         flash[:success] = "You have logged out"
         redirect_to login_path
+    end
+
+    private 
+
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "You already are logged in"
+            redirect_to root_path
+        end
     end
 
 end
