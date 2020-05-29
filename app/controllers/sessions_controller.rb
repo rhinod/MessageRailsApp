73555e586f34cde9eba341683ae8aa4873f8c6cd
rhinod <isc.damians@gmail.com>
2020-05-28 20:53:18 -0700

    Add authentication. Add flash messages show div

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
     def new
     end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Credentials are incorrect"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have logged out"
+        redirect_to login_path
+    end
+
 end
